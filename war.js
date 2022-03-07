f975356c804488e599c935de8e0b470b55a6a7de
Dua <theduall@gmail.com>
2022-03-07 15:13:35 +0200

    Event id changed

--- war.js
+++ war.js
@@ -4,17 +4,19 @@ const request = require('request'),
     entities = require('html-entities'),
     player = require('play-sound')();
 
-let lastPlayed = 0;
+let lastPlayed = {};
 
 function playNews() {
-    request.get("https://dlb-node.delfi.lt/?callback=jQuery183015372563901609904_1646482082273&eventid=98727&page=0&order=created_desc&_=1646503288059", (err, res, body) => {
+    request.get("https://dlb-node.delfi.lt/?callback=jQuery183015372563901609904_1646482082273&eventid=100593&page=0&order=created_desc&_=1646503288059", (error, res, body) => {
+        if(error) return console.log(error);
         let json = body.substr(body.indexOf("\n")).slice(0, -1); // Sanitize
         json = JSON.parse('{' + json + '');
         let message = json.messages[Object.keys(json.messages).pop()];
         message.stripped = entities.decode(striptags(message.message)); // Sanitize
         message.stripped = message.stripped.replace(/(?:https?|ftp):\/\/[\n\S]+/g, ' nuoroda'); // Remove links
-        if(lastPlayed !== message.id) {
-            lastPlayed = message.id;
+        if(lastPlayed.created !== message.created && message.origin_id !== lastPlayed.id) {
+            lastPlayed = message;
+            console.log(message);
             process.stdout.write('...\n\x1b[32m' + message.stripped + '\n');
             textToSpeech("Pyp pyp pup, karo naujiena. " + message.stripped + " Pyp pyp pup");
         } else {
